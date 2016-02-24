c245b0cc51977617c9595e29e468ff2fa924fd8e
Andrew <browner12@gmail.com>
2016-02-24 12:32:52 -0600

    update interface

--- src/UploaderInterface.php
+++ src/UploaderInterface.php
@@ -5,30 +5,52 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 interface UploaderInterface
 {
     /**
-     * upload a picture
+     * upload an image
      *
      * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
-     * @param string                                              $path
-     * @param int                                                 $id
-     * @return
+     * @param string                                              $type
+     * @param string                                              $name
+     * @param bool                                                $createOptimized
+     * @param bool                                                $createThumbnail
+     * @return array
      */
-    public function picture(UploadedFile $file, $path, $id);
+    public function image(UploadedFile $file, $type, $name, $createOptimized, $createThumbnail);
+
+    /**
+     * upload a video
+     *
+     * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
+     * @param string                                              $type
+     * @param string                                              $name
+     * @return array
+     */
+    public function video(UploadedFile $file, $type, $name);
+
+    /**
+     * upload an audio file
+     *
+     * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
+     * @param string                                              $type
+     * @param string                                              $name
+     * @return array
+     */
+    public function audio(UploadedFile $file, $type, $name);
 
     /**
      * upload a document
      *
      * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
-     * @param string                                              $path
-     * @param int                                                 $id
-     * @return
+     * @param string                                              $type
+     * @param string                                              $name
+     * @return array
      */
-    public function document(UploadedFile $file, $path, $id);
+    public function document(UploadedFile $file, $type, $name);
 
     /**
      * reprocess
      *
      * @param string $path
-     * @return
+     * @return bool
      */
     public function reprocess($path);
 }
