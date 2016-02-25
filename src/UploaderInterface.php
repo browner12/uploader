43542b0d3e024370adb1f9a12a6b622376672c2b
Andrew <browner12@gmail.com>
2016-02-25 12:43:55 -0600

    update uploader interface

--- src/UploaderInterface.php
+++ src/UploaderInterface.php
@@ -8,43 +8,43 @@ interface UploaderInterface
      * upload an image
      *
      * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
-     * @param string                                              $type
+     * @param string                                              $path
      * @param string                                              $name
      * @param bool                                                $createOptimized
      * @param bool                                                $createThumbnail
      * @return array
      */
-    public function image(UploadedFile $file, $type, $name, $createOptimized, $createThumbnail);
+    public function image(UploadedFile $file, $path, $name = null, $createOptimized = true, $createThumbnail = true);
 
     /**
      * upload a video
      *
      * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
-     * @param string                                              $type
+     * @param string                                              $path
      * @param string                                              $name
      * @return array
      */
-    public function video(UploadedFile $file, $type, $name);
+    public function video(UploadedFile $file, $path, $name = null);
 
     /**
      * upload an audio file
      *
      * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
-     * @param string                                              $type
+     * @param string                                              $path
      * @param string                                              $name
      * @return array
      */
-    public function audio(UploadedFile $file, $type, $name);
+    public function audio(UploadedFile $file, $path, $name = null);
 
     /**
      * upload a document
      *
      * @param \Symfony\Component\HttpFoundation\File\UploadedFile $file
-     * @param string                                              $type
+     * @param string                                              $path
      * @param string                                              $name
      * @return array
      */
-    public function document(UploadedFile $file, $type, $name);
+    public function document(UploadedFile $file, $path, $name = null);
 
     /**
      * reprocess
